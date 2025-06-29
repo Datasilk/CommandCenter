d30c4cee5ecb3da173e435f634f50c875b462840
Mark Entingh <entingh@gmail.com>
2025-06-29 13:18:00 -0500

    added new Visual Studio console app project

---
+++ Utility/LLM.cs
@@ -0,0 +1,31 @@
+﻿using OpenAI.Chat;
+
+
+namespace Rdio.Notifications.Utility
+{
+    public class LLM
+    {
+        public static string PrivateKey = "";
+
+        public static string Prompt(string system, string assistant, string user)
+        {
+            if (string.IsNullOrEmpty(PrivateKey))
+            {
+                throw new Exception("LLM private key is missing");
+            }
+            ChatClient client = new ChatClient("qwen-turbo-latest", new System.ClientModel.ApiKeyCredential(PrivateKey), new OpenAI.OpenAIClientOptions()
+            {
+                Endpoint = new Uri("https://dashscope-intl.aliyuncs.com/compatible-mode/v1")
+            });
+
+            List<ChatMessage> prompt = new List<ChatMessage>()
+            {
+                new SystemChatMessage(system),
+                new AssistantChatMessage(assistant),
+                new UserChatMessage(user)
+            };
+            ChatCompletion completion = client.CompleteChat(prompt);
+            return completion.Content[0].Text;
+        }
+    }
+}
